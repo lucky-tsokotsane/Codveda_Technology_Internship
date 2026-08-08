f2ae230c405000fb80e08daa48b400d3405ef811
Lucky Tsokotsane <tsokotsanel@gmail.com>
2026-08-08 16:39:38 +0000

    Enhance data analysis and visualization in task_3.py with detailed statistics and plots

--- level_1/task_3.py
+++ level_1/task_3.py
@@ -1,4 +1,27 @@
-import seaborn as sns, matplotlib.pyplot as plt, pandas as pd
+import seaborn as sns, pandas as pd, matplotlib.pyplot as plt
 
-doc = pd.read_csv(r"./Data Set For Task/iris.csv", encoding='utf-8', index_col=0)
-print(doc.head())
+dataFrame = pd.read_csv(r"../Data Set For Task/1) iris.csv")
+
+if __name__ == "__main__":
+    print(f"""{list(dict(dataFrame["species"].value_counts()).keys())[0]}\t\t\t\t\t{list(dict(dataFrame["species"].value_counts()).keys())[1]}\t\t\t\t{list(dict(dataFrame["species"].value_counts()).keys())[2]}
+Individual Average:\nsepal_length: {sum(list(dataFrame["sepal_length"])[:49+1])/50:.2f}\t\tsepal_length: {sum(list(dataFrame["sepal_length"])[49:49+50+1])/50:.2f}\t\tsepal_length: {sum(list(dataFrame["sepal_length"])[49+50+1:-1])/50:.2f}
+sepal_width: {sum(list(dataFrame["sepal_width"])[:49+1])/50:.2f}\t\tsepal_width: {sum(list(dataFrame["sepal_width"])[49:49+50+1])/50:.2f}\t\tsepal_width: {sum(list(dataFrame["sepal_width"])[49+50+1:-1])/50:.2f}
+petal_length: {sum(list(dataFrame["petal_length"])[:49+1])/50:.2f}\t\tpetal_length: {sum(list(dataFrame["petal_length"])[49:49+50+1])/50:.2f}\t\tpetal_length: {sum(list(dataFrame["petal_length"])[49+50+1:-1])/50:.2f}
+petal_width: {sum(list(dataFrame["petal_width"])[:49+1])/50:.2f}\t\tpetal_width: {sum(list(dataFrame["petal_width"])[49:49+50+1])/50:.2f}\t\tpetal_width: {sum(list(dataFrame["petal_width"])[49+50+1:-1])/50:.2f}
+
+Correlation:\n{dataFrame.corr(numeric_only=True)}
+
+Mean:\n{dataFrame.mean(numeric_only=True)}
+
+Median:\n{dataFrame.median(numeric_only=True)}
+
+Variance:\n{dataFrame.var(numeric_only=True)}""")
+
+    sns.pairplot(data=dataFrame, hue="species")
+    plt.savefig("pairplot.png")
+
+    sns.histplot(data=dataFrame)
+    plt.savefig("histplot.png")
+
+    sns.boxenplot(data=dataFrame)
+    plt.savefig("boxplot.png")
