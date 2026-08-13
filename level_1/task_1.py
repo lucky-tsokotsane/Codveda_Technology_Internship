727e050040c86933a1d87d3e34810f68fcb3919c
Lucky Tsokotsane <tsokotsanel@gmail.com>
2026-08-13 13:23:31 +0000

    Refactor web scraping function in task_1.py to improve data extraction and simplify pagination handling

--- level_1/task_1.py
+++ level_1/task_1.py
@@ -1,36 +1,22 @@
 from bs4 import BeautifulSoup
 import requests, pandas as pd
 
-def webscrape(base_site, target_site):
-    data_frame = pd.DataFrame(columns=['SHIRT NAME', 'SHIRT PRICE', 'SHIRT SPECIAL', 'SHIRT DEAL'])
-    next_page = True
+def webscrape(target_site="https://www.truworths.co.za/category/men"):
+    dataFrame = pd.DataFrame(columns=["TITLE", "BRAND", "PRICE", "IMAGE_URL"])
 
-    while next_page:
-        site = requests.get(url=target_site)
-        soup = BeautifulSoup(site.content, 'html.parser')
-        cards = soup.find_all('div', class_='gtm-product-tile css-18icsov')
+    for page_no in range(1, int(BeautifulSoup(requests.get(url=target_site).content, features="html.parser").find_all(name="a", class_="StandardPagination__PageIndicator")[-1].text) + 1):
+        site = requests.get(url=f"https://www.truworths.co.za/category/men?page={page_no}")
+        soup = BeautifulSoup(site.content, features="html.parser")
+        cards = soup.find_all(name="div", class_="single-product-tile-wrapper")
 
         for card in cards:
-            shirt_row_name = Null(card.find('p', class_='chakra-text'))
-            shirt_row_price = Null(card.find('p', class_='css-1wlfx5v'))
-            shirt_row_special = Null(card.find('div', class_='css-dx5xao'))
-            shirt_row_deal = Null(card.find('div', class_='css-qczfus'))
+            product_image_url = card.find(name="img", class_="lazy").attrs['src']
+            product_brand = card.find(name="p", class_="product-tile-brand").text
+            product_name = card.find(name="p", class_="product-tile-title").text
+            product_price = card.find(name="span", class_="product-tile-price").text
 
-            data_frame.loc[len(data_frame)] = [shirt_row_name, shirt_row_price, shirt_row_special, shirt_row_deal]
-            data_frame.to_csv(r'stored_data.csv', index=False)
-
-        if site.url != base_site+soup.find_all('a', class_='css-fko5fo')[1].attrs['href']:
-            target_site = base_site+soup.find_all('a', class_='css-fko5fo')[1].attrs['href']
-        else:
-            next_page = False
-
-
-def Null(html_tag):
-    if html_tag is None:
-        return 'NO VALUE'
-    else:
-        return html_tag.text.strip()
-
+            dataFrame.loc[len(dataFrame)] = [product_name, product_brand, product_price, product_image_url]
+            dataFrame.to_csv(path_or_buf="stored_data.csv", index=False)
 
 if __name__ == '__main__':
-    webscrape(base_site='https://www.oldkhaki.co.za', target_site='https://www.oldkhaki.co.za/c/mens-tops-tshirts?offset=0')
+    webscrape()
