a98326c5813ca720413add1dc2eec5807a087eba
Lucky Tsokotsane <tsokotsanel@gmail.com>
2026-08-16 15:21:38 +0000

    Fix feature dimensions in make_blobs and update elbow plot visualization in task_3.py

--- level_2/task_3.py
+++ level_2/task_3.py
@@ -1,6 +1,6 @@
 import sklearn as sk, matplotlib.pyplot as plt, seaborn as sns
 
-X, y = sk.datasets.make_blobs(n_samples=1000, n_features=5, random_state=0, shuffle=True)
+X, y = sk.datasets.make_blobs(n_samples=1000, n_features=2, random_state=0, shuffle=True, cluster_std=0.9, centers=5)
 WCSS = []
 
 for k in range(1, 11):
@@ -8,19 +8,18 @@ for k in range(1, 11):
     KMeans.fit(X)
     WCSS.append(KMeans.inertia_)
 
-sns.lineplot(x=range(1, 11), y=WCSS)
+sns.lineplot(x=range(1, 11), y=WCSS, markers=True)
 plt.xlabel("Number of clusters")
 plt.ylabel("WCSS")
 plt.title("Elbow plot")
-plt.show()
+plt.savefig('images/elbow_plot.jpg')
 
-
-X_tsne = sk.manifold.TSNE(n_components=2, random_state=0).fit_transform(sk.decomposition.PCA(n_components=2).fit_tranform(X))
+X_tsne = sk.manifold.TSNE(n_components=2).fit_transform(sk.decomposition.PCA(n_components=2).fit_transform(X))
 kmeans = sk.cluster.KMeans(n_clusters=len(set(y)), random_state=0)
 labels = kmeans.fit_predict(X_tsne)
 
-sns.scatterplot(x=X_tsne[:, 0], y=X_tsne[:, 1], hue=y, palette="muted")
+sns.scatterplot(x=X_tsne[:, 0], y=X_tsne[:, 1], hue=y+1, palette="muted")
 sns.scatterplot(x=kmeans.cluster_centers_[:, 0], y=kmeans.cluster_centers_[:, 1], color="black")
 plt.show()
 
-print(f"ELBOW ALGORITHM\nClusters: {len(set(y))}\nk-Means Within-Cluster Sum of Squares: {sum(WCSS)}")
+print(f"ELBOW ALGORITHM\nClusters: {len(set(y))}\nCluster points:\n{kmeans.cluster_centers_}\nk-Means Within-Cluster Sum of Squares: {sum(WCSS)}")
