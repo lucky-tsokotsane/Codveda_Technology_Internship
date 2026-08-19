a6120d7c7f73a5492e308824fe2ec7c747652e8b
Lucky Tsokotsane <tsokotsanel@gmail.com>
2026-08-19 03:32:59 +0000

    Refactor task_2.py to implement sentiment analysis using MLPClassifier and update data preprocessing steps

--- level_3/task_2.py
+++ level_3/task_2.py
@@ -1,18 +1,24 @@
-import pandas as pd
+import pandas as pd, nltk
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.naive_bayes import GaussianNB, MultinomialNB
+from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import f1_score
 
-dataset = pd.read_csv(filepath_or_buffer='Data Set For Task/3) Sentiment dataset.csv').copy()
-dataset.drop(labels=['Unnamed: 0.1', 'Unnamed: 0', 'Timestamp', 'User', 'Platform', 'Hashtags', 'Retweets', 'Likes', 'Country', 'Year', 'Month', 'Day', 'Hour'], axis=1, inplace=True)
-x_Text_trainData, x_Text_testData, y_Sentiment_trainData, y_Sentiment_testData = train_test_split(dataset["Text"], dataset["Sentiment"], random_state=42, test_size=0.2, train_size=0.8)
+dataset = pd.read_csv('Data Set For Task/Nostalgic_Sentiment_Analysis_of_YouTube_Comments_Data.csv').copy()
+x_Text_trainData, x_Text_testData, y_Sentiment_trainData, y_Sentiment_testData = train_test_split(dataset["comment"], dataset["sentiment"], random_state=42, test_size=0.2, train_size=0.8)
 
-vectorizer = TfidfVectorizer()
+stemmer = nltk.stem.PorterStemmer()
+def Stemmer(comment):
+    return [stemmer.stem(word) for word in comment.split()]
+
+nltk.download('stopwords')
+stop_words = nltk.corpus.stopwords.words('english')
+
+vectorizer = TfidfVectorizer(stop_words=stop_words, analyzer=Stemmer)
 vectors_train = vectorizer.fit_transform(x_Text_trainData)
 vectors_test = vectorizer.transform(x_Text_testData)
 
-classifier = GaussianNB()
-classifier.fit(vectors_train.toarray(), y_Sentiment_trainData)
-y_pred = classifier.predict(vectors_test.toarray())
-print(f1_score(y_Sentiment_testData, y_pred, average=None))
+MLClassifier = MLPClassifier(max_iter=1500, learning_rate='adaptive')
+MLClassifier.fit(vectors_train.toarray(), y_Sentiment_trainData)
+MLVector_test = MLClassifier.predict(vectors_test.toarray())
+print("Model Accuracy/Re-call_score:", MLClassifier.score(vectors_test.toarray(), y_Sentiment_testData), "f1-score:", f1_score(y_Sentiment_testData, MLVector_test, average='weighted'))
